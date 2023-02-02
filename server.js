316f9f9542da81aafab77a9d54320830b77a6580
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-02-01 21:48:39 -0700

    add route to notes

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
 
 
 
