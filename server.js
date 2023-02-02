b5c6e036a34585f50096766697a4be669e011a65
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-02-01 18:26:39 -0700

    set up dependencies, expressand PORT

---
+++ server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
